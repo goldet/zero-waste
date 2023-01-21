0929e516053c67987ed968e3a6fd33f0af589756
Golde Tischler <golde.tischler@gmail.com>
2023-01-21 19:52:17 +0100

    -installed expresss

---
+++ model/database.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+//??
+/* const fs = require("fs"); */
+//??
+/* import { dirname } from 'path';
+const __dirname = dirname(__filename);
+ */
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
+
+const con = mysql.createConnection({
+  host: DB_HOST || "127.0.0.1",
+  user: DB_USER || "root",
+  password: DB_PASS,
+  database: DB_NAME || "products",
+  multipleStatements: true
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  let sql = fs.readFileSync(_dirname + "init.sql").toString(); 
+  con.query(sql, function(err, result) { 
+    if (err) throw err;
+    console.log("Table creation 'items' was successful!"); 
+
+  /* let sql =
+    "DROP TABLE if exists items; CREATE TABLE items(id INT NOT NULL AUTO_INCREMENT, text VARCHAR(40) not null, complete BOOLEAN, PRIMARY KEY (id));"; */
+ /*  con.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("Table creation `items` was successful!"); */
+
+    console.log("Closing...");
+  });
+
+  con.end();
+});
