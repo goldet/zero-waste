0c624c1d2a2fd34b72ba4664dd4914ef24202b2f
Golde Tischler <golde.tischler@gmail.com>
2023-01-23 11:11:12 +0100

    more data in products table

--- model/init_db.sql
+++ model/init_db.sql
@@ -3,12 +3,15 @@
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(20) NOT NULL,
     name VARCHAR(20) NOT NULL,
     type VARCHAR(20) NOT NULL,
-    amount FLOAT(3, 0) NOT NULL,
-    phone_number VARCHAR(10) NOT NULL
+    description VARCHAR (1000), 
+    amount FLOAT(3, 0) NOT NULL, 
+    phone_number VARCHAR(10) NOT NULL,
+    zip_code VARCHAR(10) NOT NULL
 );
 
-INSERT INTO products (name, type, amount, phone_number)
-VALUES ('tomatoes', 'fruits', 5, '697831082');
+INSERT INTO products (firstname, name, type, description, amount, phone_number)
+VALUES ('Golde', 'tomatoes', 'fruits', 'a few days old, some are very soft', 5, '697831082');
 
