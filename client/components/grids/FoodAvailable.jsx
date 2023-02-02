c15f419c871ed6b8be9082883bc3f933b8a528cf
Golde Tischler <golde.tischler@gmail.com>
2023-02-02 12:20:37 +0100

    seperated search bar component and render in grid

--- client/components/grids/FoodAvailable.jsx
+++ client/components/grids/FoodAvailable.jsx
@@ -9,7 +9,22 @@ const FoodAvailable = () => {
   const [error, setError] = useState(null);
 
   //state to hold the products with the matching zip code
+  const [zipCodeProducts, setZipCodeProducts] = useState(null)
   //add function to add the zip code to this component 
+  const addZipCode = ((searchInput) => {
+    getProductByZipCode(searchInput)
+ })
+
+ const getProductByZipCode = async (searchInput) => { 
+      
+  const response = await fetch(`${BASE_URL}/products?zip_code=${searchInput}`); 
+     console.log(response);
+     const data = await response.json();
+     console.log(data);
+     const products= data.product;
+     setZipCodeProducts(products);
+    
+    };
 
   useEffect(() => {
     const getProducts = async () => {
@@ -37,6 +52,7 @@ const FoodAvailable = () => {
     } catch (error) {
       setError("Oops! Something went wrong. Try again later");
     }
+
   };
 
   return (
@@ -44,7 +60,25 @@ const FoodAvailable = () => {
       <h1 className="text-4xl text-center pt-5 pb-10">Food to share!</h1>
 
      {/*  search bar with ternary operator to show either just the zip codes or all products  */}
-       <SearchBarNew />
+      
+     
+       <SearchBarNew addZipCode={addZipCode}/>
+       {zipCodeProducts && 
+   zipCodeProducts.map((product) => (
+    <div key = {product.id}>
+       <div className="container">
+                {" "}
+                Product: {product.name} <br /> Type: {product.type} <br />{" "}
+                Description: <br /> {product.description} <br /> Amount:{" "}
+                {product.amount} <br /> Contact {product.firstname}:{" "}
+                {product.phone_number} <br /> Zip code: {product.zip_code}
+              </div>
+    </div>
+   ))}
+
+
+
+
 
       {products &&
         products.map((product) => (
--- client/components/grids/FoodNeeds.jsx
+++ client/components/grids/FoodNeeds.jsx
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from "react";
+import SearchBarNew from "./SearchBarNew";
 
 const BASE_URL = "http://localhost:5000";
 
 const FoodNeeds = () => {
   const [products, setProducts] = useState(null);
   const [error, setError] = useState(null);
+
+   //state to hold the products with the matching zip code
+   const [zipCodeProducts, setZipCodeProducts] = useState(null)
+   //add function to add the zip code to this component 
+   const addZipCode = ((searchInput) => {
+     getProductByZipCode(searchInput)
+  })
+ 
+  const getProductByZipCode = async (searchInput) => { 
+       
+   const response = await fetch(`${BASE_URL}/products?zip_code=${searchInput}`); 
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      const products= data.product;
+      setZipCodeProducts(products);
+     
+     };
 
   useEffect(() => {
     const getProducts = async () => {
@@ -40,6 +59,24 @@ const FoodNeeds = () => {
   return (
     <>
       <h1 className="text-4xl text-center pt-5 pb-10">Food Needs!</h1>
+
+           {/*  search bar with ternary operator to show either just the zip codes or all products  */}
+           <SearchBarNew addZipCode={addZipCode}/>
+       {zipCodeProducts && 
+   zipCodeProducts.map((product) => (
+    <div key = {product.id}>
+       <div className="container">
+                {" "}
+                Product: {product.name} <br /> Type: {product.type} <br />{" "}
+                Description: <br /> {product.description} <br /> Amount:{" "}
+                {product.amount} <br /> Contact {product.firstname}:{" "}
+                {product.phone_number} <br /> Zip code: {product.zip_code}
+              </div>
+    </div>
+   ))} 
+
+
+
       {products &&
         products.map((product) => (
           <div key={product.id}>
