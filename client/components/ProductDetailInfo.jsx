10a486f6c1041519aa7839f1be34762e17395740
Golde Tischler <golde.tischler@gmail.com>
2023-02-22 00:33:21 +0100

    get item by id func in product service class

--- client/components/ProductDetailInfo.jsx
+++ client/components/ProductDetailInfo.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import services from "./services";
 import ProductService from "./services/ProductService";
 
-
 const BASE_URL = "http://localhost:5000";
 
 //component that shows an individual item
@@ -13,25 +12,19 @@ const ProductDetailInfo = () => {
   const [product, setProduct] = useState(null);
   const [error, setError] = useState(null);
 
-
-  //get product by id 
   useEffect(() => {
-    const getProducts = async () => {
-      if (id) { // null check
+    const getProduct = async () => {
+      if (id) {
         try {
-          const response = await fetch(`${BASE_URL}/products/${id}`);
-          const data = await response.json();
-          const product = data[0];
-          setProduct(product);
-        } catch(error) {
+          const product = await services.productService.fetchOne(id);
+          setProduct(product[0]);
+        } catch (error) {
           setError(error);
         }
       }
     };
-    getProducts();
+    getProduct();
   }, [id]);
-
-
 
   // Show error & loading states
   let state = <></>;
@@ -41,8 +34,6 @@ const ProductDetailInfo = () => {
 
   return (
     <div className="page-container">
-      
-
       {/* testing */}
       {product && <h3 className="">{product.id}</h3>}
     </div>
