54ebe0a31403bf9088c953a4cf5408e219913530
Golde Tischler <golde.tischler@gmail.com>
2023-02-22 12:22:57 +0100

    img change fix for product detail

--- client/components/ProductDetailInfo.jsx
+++ client/components/ProductDetailInfo.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import GridButtons from "./forms/GridButtons";
 import services from "./services";
 import { MdLocationOn } from "react-icons/md";
+import Link from "next/link";
 
 const BASE_URL = "http://localhost:5000";
 
@@ -38,21 +39,39 @@ const ProductDetailInfo = () => {
       {product ? (
         <article className="main">
           <div className="container1">
-            <GridButtons />
+            {/*  {product.needed === true ? (<Link className="back-link" href="/grids/foodneeds">BACK</Link>) : (<Link className="back-link"  href="/grids/foodavailable">BACK</Link>)} */}
+
+            <GridButtons className="back-link" />
             <div className="spacer-20"></div>
             {state}
             <div className="item-detail-card">
               <div className="item-detail-grid">
-                {product.image_path === null ? (
+                {product.image_path !== null ? (
+                  <img
+                    className="w-full h-auto"
+                    src={`http://localhost:5000/images/${product.image_path}`}
+                    alt={`photo of ${product.name}`}
+                  />
+                ) : product.image_path === null &&
+                  product.type === "vegetables" ? (
                   <img
                     className="item-detail-img"
-                    src={`https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80`}
+                    src={`https://images.unsplash.com/photo-1552245504-5b1e10c5c04a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80`}
+                  />
+                ) : product.image_path === null && product.type === "fruits" ? (
+                  <img
+                    className="item-detail-img"
+                    src={`https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1239&q=80`}
+                  />
+                ) : product.image_path === null && product.type === "meat" ? (
+                  <img
+                    className="item-detail-img"
+                    src={`https://images.unsplash.com/photo-1552962402-6bafde66a37c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80`}
                   />
                 ) : (
                   <img
                     className="item-detail-img"
-                    src={`http://localhost:5000/images/${product.image_path}`}
-                    alt={`photo of ${product.name}`}
+                    src={`https://images.unsplash.com/photo-1518291344630-4857135fb581?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80`}
                   />
                 )}
                 <div className="item-detail-text">
@@ -69,6 +88,7 @@ const ProductDetailInfo = () => {
                     <p className="item-detail-heading">ITEM DESCRIPTION</p>
                     <p>{product.description}</p>
                     <p className="item-detail-heading">CONTACT INFO</p>
+                    <p>{product.firstname}</p>
                     <p>{product.phone_number}</p>
                   </div>
                   <div className="horizontal-line">
